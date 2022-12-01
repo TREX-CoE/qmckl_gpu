a1b115b21dae1206ad672a9937a5c5a619e6a1be
Anthony Scemama <scemama@irsamc.ups-tlse.fr>
2022-12-01 09:44:06 +0100

    Update autogen.sh
    Revert autogen.sh because https set in .gitmodule file

--- autogen.sh
+++ autogen.sh
@@ -9,5 +9,5 @@ install_qmckl()
 }
 
 test -f qmckl/configure || \
- 	(git config --global url."https://github.com/".insteadOf "git@github.com:" && git submodule update --init && install_qmckl)
+ 	(git submodule update --init && install_qmckl)
 autoreconf -is
