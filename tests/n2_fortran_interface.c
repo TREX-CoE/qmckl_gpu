a5142aa1f52aeb3cc65d86f91504ecc58399ff85
Aurélien Delval <a.delval87@gmail.com>
2023-07-27 04:29:59 +0200

    Add n2 Fortran interface in tests

---
+++ tests/n2_fortran_interface.c
@@ -0,0 +1,32 @@
+// This file contains C functions that read arrays from n2.h, and that are
+// meant to be called from the Fortran test
+
+#include <inttypes.h>
+#include "n2.h"
+
+int64_t get_n2_nucl_num() { return n2_nucl_num; }
+
+double* get_n2_charge() { return &(n2_charge[0]); }
+
+
+int64_t get_n2_elec_up_num() { return n2_elec_up_num; }
+int64_t get_n2_elec_dn_num() { return n2_elec_dn_num; }
+int64_t get_n2_elec_num() { return n2_elec_num; }
+int64_t get_n2_walk_num() { return n2_walk_num; }
+
+
+double* get_n2_elec_coord() { return &(n2_elec_coord[0][0][0]); }
+
+int64_t get_n2_type_nucl_num() { return n2_type_nucl_num; }
+int64_t get_n2_aord_num() { return n2_aord_num; }
+int64_t get_n2_bord_num() { return n2_bord_num; }
+int64_t get_n2_cord_num() { return n2_cord_num; }
+int64_t get_n2_dim_cord_vec() { return n2_dim_cord_vec; }
+
+int64_t* get_n2_type_nucl_vector() { return &(n2_type_nucl_vector[0]); }
+
+double* get_n2_aord_vector() { return &(n2_aord_vector[0][0]); }
+double* get_n2_bord_vector() { return &(n2_bord_vector[0]); }
+double* get_n2_cord_vector() { return &(n2_cord_vector[0][0]); }
+double* get_n2_cord_vector_full() { return &(n2_cord_vector_full[0][0]); }
+double* get_n2_lkpm_of_cindex() { return &(n2_lkpm_of_cindex[0][0]); }
